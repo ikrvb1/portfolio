746d10f25e9fdf336c11fa92898133fd64e5bfc7
ikrvb1 <ibustillos@ucsd.edu>
2025-01-25 21:50:52 -0800

    lab 03 - step 03
    removed <nav> and remade it using JS

--- global.js
+++ global.js
@@ -4,10 +4,29 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a")
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/ikrvb1', title: 'GitHub' }
+ ];
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-  );
+let nav = document.createElement('nav');
+document.body.prepend(nav);
 
-  currentLink?.classList.add("current");
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+  }
+
