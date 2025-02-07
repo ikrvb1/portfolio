cf107f438b758153b140afcc3985f7b597b444f6
ikrvb1 <ibustillos@ucsd.edu>
2025-02-07 15:49:45 -0800

    Lab 05 - Step 02
    added legend to pie chart

--- projects/projects.js
+++ projects/projects.js
@@ -15,8 +15,15 @@ if (projects && Array.isArray(projects)) {
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let data = [1, 2, 3, 4, 5, 5];
-let sliceGenerator = d3.pie();
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 
@@ -28,3 +35,9 @@ arcs.forEach((arc, idx) => {
     .attr('fill', colors(idx));
 })
 
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+    legend.append('li')
+          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+})
